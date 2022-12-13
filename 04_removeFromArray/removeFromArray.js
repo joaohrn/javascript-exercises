2efe9b9ef83dfac1631fe095f68fb7aafe57a5e6
joaohrn <joaohrn0@gmail.com>
2022-12-13 12:43:48 -0300

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...toRemove) {
+  for (item of toRemove) {
+    let index = array.indexOf(item);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
