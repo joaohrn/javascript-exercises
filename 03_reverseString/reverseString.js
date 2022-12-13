dd58b76b73ff05a27d7cbfa1c86fe61c47133770
joaohrn <joaohrn0@gmail.com>
2022-12-13 12:19:56 -0300

    Solve 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let tempArray = string.split("");
+  let newString = "";
+  for (let i = tempArray.length - 1; i >= 0; i--) {
+    newString += tempArray[i];
+  }
+  return newString;
 };
 
 // Do not edit below this line
