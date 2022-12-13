8df28e97c589517cf9465135296804fb9e2be8a6
joaohrn <joaohrn0@gmail.com>
2022-12-13 12:11:22 -0300

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, amount) {
+  let newString = "";
+  if (amount < 0) {
+    newString = "ERROR";
+  } else {
+    for (let i = 1; i <= amount; i++) {
+      newString += string;
+    }
+  }
+  return newString;
 };
 
 // Do not edit below this line
