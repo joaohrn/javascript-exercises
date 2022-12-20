76e373191f969aa9f40abbdbc15998d5ee3b9fba
joaohrn <joaohrn0@gmail.com>
2022-12-20 12:54:26 -0300

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  for (num of arr) {
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  let prod = 1;
+  for (num of arr) {
+    prod = prod * num;
+  }
+  return prod;
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+  let res = 1;
+  for (let i = 1; i <= exp; i++) {
+    res *= base;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +48,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
