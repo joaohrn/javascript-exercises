c3d05ba679875403f12d402e43c280a4f641c1ac
joaohrn <joaohrn0@gmail.com>
2022-12-21 11:56:29 -0300

    Add solution to 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let arr = str.toLowerCase().split(" ").join("").split("");
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      arr[i] == "!" ||
+      arr[i] == "." ||
+      arr[i] == "?" ||
+      arr[i] == "," ||
+      arr[i] == ";" ||
+      arr[i] == ":"
+    ) {
+      arr.splice(i, 1);
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[arr.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
