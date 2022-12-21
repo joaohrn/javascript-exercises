e52c2795637fd7c0d01f77c6cd000bea4e448657
joaohrn <joaohrn0@gmail.com>
2022-12-21 12:15:33 -0300

    Add Solution to 10 and 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+  if (index < 0) {
+    return "OOPS";
+  }
+  let fib = [];
+  for (let i = 0; i < index; i++) {
+    if (fib.length === 0) {
+      fib.push(0, 1);
+    } else {
+      fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+    }
+  }
+  return fib[fib.length - 1];
 };
 
 // Do not edit below this line
