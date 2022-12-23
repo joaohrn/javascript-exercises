407edfa42dc5c83285bd2da9397a2c7d0b0da223
joaohrn <joaohrn0@gmail.com>
2022-12-23 10:29:23 -0300

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((oldest, person) => {
+    return getAge(person.yearOfBirth, person.yearOfDeath) >
+      getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+      ? person
+      : oldest;
+  });
+};
+const getAge = (birth, death) => {
+  if (!death) {
+    return new Date().getFullYear() - birth;
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
